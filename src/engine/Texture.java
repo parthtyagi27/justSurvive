bc2720907dc7482a7a68c2254278ee34e1993ade
parthtyagi27 <44475459+parthtyagi27@users.noreply.github.com>
2019-02-24 17:57:59 -0600

    Updated texture file for multiple object support

--- src/engine/Texture.java
+++ src/engine/Texture.java
@@ -8,6 +8,7 @@ import javax.imageio.ImageIO;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
+import org.lwjgl.system.MemoryUtil;
 
 public class Texture
 {
@@ -57,6 +58,97 @@ public class Texture
 		
 	}
 	
+	public Texture(BufferedImage bufferedImage)
+	{
+		try
+		{
+			width = bufferedImage.getWidth();
+			height = bufferedImage.getHeight();
+			
+			int[] pixels = new int[width * height];
+	        bufferedImage.getRGB(0, 0, width, height, pixels, 0, width);
+
+	        /* Put pixel data into a ByteBuffer */
+	        ByteBuffer buffer = MemoryUtil.memAlloc(width * height * 4);
+	        for (int i = 0; i < height; i++) {
+	            for (int j = 0; j < width; j++) {
+	                /* Pixel as RGBA: 0xAARRGGBB */
+	                int pixel = pixels[i * width + j];
+	                /* Red component 0xAARRGGBB >> 16 = 0x0000AARR */
+	                buffer.put((byte) ((pixel >> 16) & 0xFF));
+	                /* Green component 0xAARRGGBB >> 8 = 0x00AARRGG */
+	                buffer.put((byte) ((pixel >> 8) & 0xFF));
+	                /* Blue component 0xAARRGGBB >> 0 = 0xAARRGGBB */
+	                buffer.put((byte) (pixel & 0xFF));
+	                /* Alpha component 0xAARRGGBB >> 24 = 0x000000AA */
+	                buffer.put((byte) ((pixel >> 24) & 0xFF));
+	            }
+	        }
+	        /* Do not forget to flip the buffer! */
+	        buffer.flip();
+			
+			id = GL11.glGenTextures();
+			
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+			
+			GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, width, height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		
+	}
+	
+	public Texture(ByteBuffer pixels, int w, int h)
+	{
+		try
+		{
+			
+
+		
+			
+//			pixels.flip();
+			
+			id = GL11.glGenTextures();
+			width = w;
+			height = h;
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+			
+			GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, width, height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, pixels);
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+		
+		
+	
+	
+	
+	public Texture(int textureId, ByteBuffer pixels, int charWidth, int charHeight)
+	{
+		id = textureId;
+		width = charWidth;
+		height = charHeight;
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
+		GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
+		GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+		
+		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, width, height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, pixels);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
+	}
+
 	public void bind()
 	{
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
